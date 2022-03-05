c58d84eb0660b8184980da8950e1c4e3d9ee6aae
daniela <moreno.tobar.daniela@gmail.com>
2022-03-04 21:30:56 -0500

    start

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Carousel from './components/containers/Carousel';
+import Navbar from './components/containers/Navbar';
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <Carousel/>   
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/containers/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+export default function Navbar() {
+  return (
+    <div>
+      <nav className="relative flex flex-wrap items-center justify-between py-2 bg-white mb-3 shadow-lg">
+        <div className="px-20">
+              <img
+                  className="h-20 w-20"
+                  src="https://i.ibb.co/zGyzkYm/sticker-pets-club.png"
+                  alt="Logo"
+                />
+        </div>        
+      </nav>
+    </div>
+  );
+}
