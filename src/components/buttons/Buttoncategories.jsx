13c31df51a013587d5fec76b8c2411e9c7e315e7
daniela <moreno.tobar.daniela@gmail.com>
2022-03-09 13:25:02 -0500

    buttons responsive

--- src/components/buttons/Buttoncategories.jsx
+++ src/components/buttons/Buttoncategories.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 export default function Buttoncategories() {
   return (
     <div className='mt-8 flex gap-3 justify-center items-center w-full'>
-        <button className="btn btn-info items-center flex justify-center sm:w-32 w-full h-10 py-3">
+        <button className="btn btn-info items-center flex justify-center sm:w-32 h-10 py-3">
             <p className="text-base font-medium leading-none text-white">Alimento</p>
         </button>
 
-        <button className="btn btn-info items-center flex justify-center sm:w-32 w-full h-10 py-3">
+        <button className="btn btn-info items-center flex justify-center sm:w-32 h-10 py-3">
             <p className="text-base font-medium leading-none text-white">Cuidado</p>
         </button>
     </div>
