b65c334afd16eb3eed6d52231d78eee24ce7e38f
daniela <moreno.tobar.daniela@gmail.com>
2022-03-16 17:41:11 -0500

    img

--- src/data/categories.js
+++ src/data/categories.js
@@ -20,7 +20,7 @@ const data =
             "peso": "1.5 KG",
             "description": "Alimento super premium para perros, elaborado en Colombia bajo los mas altos estandares de calidad, sus excelentes fuentes de proteína garantizan una excelente asimilación de sus nutrientes. Enriquecido con vitaminas y minerales de fácil absorción. Contiene omegas tres y seis para una piel sana y un pelaje bello y brillante.",
             "price": 27750,
-            "image": "https://i.ibb.co/NyB1WXd/p1.jpg",
+            "image": "https://raw.githubusercontent.com/DanielaTob/img/main/p1.jpg",
             "category": "Alimento"
         },
 
@@ -30,7 +30,7 @@ const data =
             "peso": "1.5 KG",
             "description": "El concentrado para gatos Agility Gold es un concentrado ideal para suministrar en todas las etapas de la vida de los gatos. Su fórmula libre de granos será ideal para proporcionar una alimentación adecuada y una digestión más rápida. Contiene niveles equilibrados de minerales que favorecerán la salud renal y es perfecta para evitar la formación de las bolas de pelo.",
             "price": 33000,
-            "image": "https://i.ibb.co/FBM0BBv/p2.jpg",
+            "image": "https://raw.githubusercontent.com/DanielaTob/img/main/p2.jpg",
             "category": "Alimento"
         },
 
@@ -41,7 +41,7 @@ const data =
             "peso": "1.5 KG",
             "description": "Es un concentrado para gatos, cuya fórmula está hecha con carne de verdad, así que sus croquetas serán sabrosas y muy palatables para tu gato. Su fórmula esta especialmente diseñada para controlar las bolas de pelo y favorecer su digestión gracias a su alto nivel de fibra.",
             "price": 19800,
-            "image": "https://i.ibb.co/hDRFQzS/p3.jpg",
+            "image": "https://raw.githubusercontent.com/DanielaTob/img/main/p3.jpg",
             "category": "Alimento"
         },
 
@@ -52,7 +52,7 @@ const data =
             "peso": "1 KG",
             "description": "Max cat adulto pollo y arroz, Sin la adición de colorantes, el alimento max cat adultos pollo & arroz trae ingredientes seleccionados por especialistas para proporcionarle una alimentación rica y balanceada a tu gatito.",
             "price": 19000,
-            "image": "https://i.ibb.co/RDMwsrz/p4.jpg",
+            "image": "https://raw.githubusercontent.com/DanielaTob/img/main/p4.jpg",
             "category": "Alimento"
         },
 
@@ -63,7 +63,7 @@ const data =
             "peso": "2 KG",
             "description": "Chunky Perro Adulto Pollo es un alimento para perros adultos de todas las razas, hecho con pollo de verdad. Esta dieta esta indicada para perros adultos y hasta los 7 años, con un delicioso sabor y olor a pollo que tu mascota no podrá resistir.",
             "price": 15700,
-            "image": "https://i.ibb.co/x3DvvPg/p5.jpg",
+            "image": "https://raw.githubusercontent.com/DanielaTob/img/main/p5.jpg",
             "category": "Alimento"
         },
 
@@ -74,7 +74,7 @@ const data =
             "peso": "2 KG",
             "description": "Chunky cachorros pollo Comida para perros entre los 2 y 12 meses, elaborada con proteínas de excelente calidad y un sabor exquisito con un alto nivel de proteína proveniente de carne de pollo real que favorecerán su salud en general, la digestión, protegerán el tracto gastrointestinal y el mantenimiento óptimo de la piel y el pelaje.",
             "price": 18500,
-            "image": "https://i.ibb.co/b2C5FGQ/p6.jpg",
+            "image": "https://raw.githubusercontent.com/DanielaTob/img/main/p6.jpg",
             "category": "Alimento"
         },
 
@@ -88,7 +88,7 @@ const data =
             "peso": "1000 Mlg",
             "description": "Es un poderoso medicamento que trata y controla las infestaciones en perros de pulgas y garrapatas. También previene la transmisión de Bebesia Canis 12 semanas. Trata la demodicosis y sarna y infestaciones por ácaros. Peso entre 20-40 Kilogramos.",
             "price": 105100,
-            "image": "https://i.ibb.co/nRHxMqW/id7.jpg",
+            "image": "https://raw.githubusercontent.com/DanielaTob/img/main/id7.jpg",
             "category": "Cuidado"
         },
 
@@ -98,7 +98,7 @@ const data =
             "peso": "500 Mlg",
             "description": "Bravecto mata las pulgas adultas y está indicado para el tratamiento y la prevención de infestaciones por pulgas (Ctenocephalides felis y C Canis) y para el tratamiento y el control de infestaciones por garrapatas (Rhipicephalus sanguineus) durante 12 semanas en perros y cachorros a partir de los 2 meses de edad.",
             "price": 82900,
-            "image": "https://i.ibb.co/fMfNYsK/id8.jpg",
+            "image": "https://raw.githubusercontent.com/DanielaTob/img/main/id8.jpg",
             "category": "Cuidado"
         },
 
@@ -108,7 +108,7 @@ const data =
             "peso": "0.4 ML",
             "description": "Bravecto Antiparasitario Spot on Perros de 2 a 4.5 Kg Solución tópica 112.5 mg. Este es un antiparasitario ideal para matar pulgas y garrapatas durante 12 semanas, puede usarse como tratamiento para controlar la dermatitis alérgica o las picaduras de las pulgas. Ideal para perros pequeños con peso de 2 a 4.5 kg.",
             "price": 78400,
-            "image": "https://i.ibb.co/60JWt7k/id9.jpg",
+            "image": "https://raw.githubusercontent.com/DanielaTob/img/main/id9.jpg",
             "category": "Cuidado"
         },
 
@@ -118,7 +118,7 @@ const data =
             "peso": "0.4 ML",
             "description": "Antiparasitario de acción inmediata y persistente ideal para matar pulgas y garrapatas durante semanas, apto para usarse como tratamiento para controlar la dermatitis alérgica o las picaduras de las pulgas. Ideal para gatos pequeños con peso de 1.2 a 2.8 kg.",
             "price": 78400,
-            "image": "https://i.ibb.co/Z23fzMF/id10.jpg",
+            "image": "https://raw.githubusercontent.com/DanielaTob/img/main/id10.jpg",
             "category": "Cuidado"
         },
 
@@ -128,7 +128,7 @@ const data =
             "peso": "0.4 ML",
             "description": "Antiparasitario de acción inmediata y persistente ideal para matar pulgas y garrapatas durante semanas, apto para usarse como tratamiento para controlar la dermatitis alérgica o las picaduras de las pulgas. Ideal para gatos pequeños con peso de 2.8 a 6.2 Kg",
             "price": 85100,
-            "image": "https://i.ibb.co/9N2sQd1/id11.jpg",
+            "image": "https://raw.githubusercontent.com/DanielaTob/img/main/id11.jpg",
             "category": "Cuidado"
         }
 
